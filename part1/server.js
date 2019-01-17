ae02a044a28649a31b5471b4a25e21aea8fc34b2
Gregory Olivas <azdmnd6021998@gmail.com>
2019-01-17 00:05:44 -0700

    finalized changes and completed assignment

--- part1/server.js
+++ part1/server.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var path = require('path')
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+
+app.get('/hello', function (req, res) {
+  res.send("Hello!");
 });
 
-app.use(function(req, res) {
+app.post("/create/:name", function (req, res) {
+  let namesJson = {
+    "id": 0,
+    "name": req.params.name
+  }
+  res.json(namesJson)
+})
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, './index.html'))
+})
+
+app.get("/verify/:age", function(req, res) {
+  if(req.params.age >= 13){
+    res.sendStatus(200)
+  }else if(req.params.age < 13){
+    res.sendStatus(403)
+  }
+})
+
+app.use(function (req, res) {
   res.sendStatus(404);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Listening on port', port);
 });
--- part2/server.js
+++ part2/server.js
@@ -1,15 +1,52 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+const fs = require('fs')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.post('/create/:name/:age', function(req, res) {
+  fs.readFile("./storage.json", "utf8", function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
+
+    console.log(req.body);
+    usersArr.push(req.body);
+
+    fs.writeFile("./storage.json", JSON.stringify(usersArr), function(err){
+      if(err) throw err;
+
+      res.sendStatus(200);
+
+    })
+
+  })
 });
 
-app.use(function(req, res) {
-  res.sendStatus(404);
+app.get('/', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
+
+    res.json(usersArr);
+  })
 });
 
-app.listen(port, function() {
+app.get('/users/:name', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
+
+    for(let i = 0; i<usersArr.length; i++){
+      if(usersArr[i].name == req.params.name){
+        res.json(usersArr[i]);
+        return;
+      }
+    }
+    res.sendStatus(400);
+  })
+});
+
+app.listen(port, function () {
   console.log('Listening on port', port);
 });
